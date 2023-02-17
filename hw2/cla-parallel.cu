75494d7a01e449e2ff0a64110727fe87317d828d
Truong Tommy <79810374+TruongTommy99@users.noreply.github.com>
2023-02-17 08:57:18 -0500

    new

--- hw2/cla-parallel.cu
+++ hw2/cla-parallel.cu
@@ -380,17 +380,11 @@ void cla()
   //       assignment description.
   /***********************************************************************************************************/
     compute_gp();
-    cudaDeviceSynchronize();
     compute_group_gp();
-    cudaDeviceSynchronize();
     compute_section_gp();
-    cudaDeviceSynchronize();
     compute_super_section_gp();
-    cudaDeviceSynchronize();
     compute_super_super_section_gp();
-    cudaDeviceSynchronize();
     compute_super_super_section_carry();
-    cudaDeviceSynchronize();
     compute_super_section_carry();
     compute_section_carry();
     compute_group_carry();
